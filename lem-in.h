2eec8b54bffdf76bd3b72405bc8969739851b922
Kalista Bessa <kbessa@oa-c1.21-school.ru>
2019-11-12 19:45:18 +0300

    test

---
+++ lem-in.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lem-in.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbessa <kbessa@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/11 15:03:29 by kbessa            #+#    #+#             */
+/*   Updated: 2019/11/11 15:05:19 by kbessa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEM_IN_LEM_IN_H
+# define LEM_IN_LEM_IN_H
+# include "libft/includes/libft.h"
+# include "libft/includes/get_next_line.h"
+# include "libft/includes/ft_printf.h"
+
+typedef	struct s_lem
+{
+	int antnum;
+}t_lem;
+
+#endif
