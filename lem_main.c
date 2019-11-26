d0be908f4a7f734a9232837280f3baf5f54f1a8e
Aleksandr <47306917+FailError@users.noreply.github.com>
2019-11-26 22:58:18 +0300

    qwe

--- lem_main.c
+++ lem_main.c
@@ -6,7 +6,7 @@
 /*   By: kbessa <kbessa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/11 15:05:49 by kbessa            #+#    #+#             */
-/*   Updated: 2019/11/25 21:13:17 by kbessa           ###   ########.fr       */
+/*   Updated: 2019/11/26 19:38:56 by kbessa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,19 +27,21 @@ t_rooms	*ft_create(void)
 	return (rom);
 }
 
-t_rooms *ft_push_back(t_rooms *rooms, char *str)
+t_rooms *ft_push_back(t_rooms *head, char *str)
 {
-	t_rooms *ptr = rooms;
+	t_rooms *ptr = head;
 	while(ptr->next != NULL)
 		ptr = ptr->next;
-	t_rooms *newnode = ft_create();//(t_rooms *)malloc(sizeof(t_rooms));
+	t_rooms *newnode = ft_create();
 	ptr->next = newnode;
 	newnode->next = NULL;
 	newnode->prev = ptr;
+	head->prev = newnode;
 
 	newnode->name = (char *) malloc(sizeof(char) * 5);
 	ft_strlcat(newnode->name, str, 5);
-	return (rooms);
+	newnode->name[4] = '\0';
+	return (head);
 }
 
 void ft_parser(int argc, char **argv)
@@ -47,22 +49,14 @@ void ft_parser(int argc, char **argv)
 	char *str = NULL;
 	int fd;
 	t_rooms *rooms = ft_create();
-	//t_rooms *head = rooms;
 	int i = 0;
 	fd = open(argv[1], O_RDONLY);
 	while (get_next_line(fd, &str) == 1)
 	{
-		while (str[4] && str[4] == ' ')
+		if (str[4] && str[4] == ' ')
 		{
 			ft_push_back(rooms, str);
-
-
-
-
-//			t_rooms *rooms1 = (t_rooms *)malloc(sizeof(t_rooms));
-//			rooms1->name = (char *) malloc(sizeof(char) * 5);
-//			ft_strlcat(rooms1->name, str, 5);
-//			rooms->next = NULL;
+			free(str);
 		}
 	}
 	if (ft_strcmp(str, "##start") == 0)
