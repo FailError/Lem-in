f46df94e38f667e0bd58306563070fab7a0b9d05
Aleksandr <47306917+FailError@users.noreply.github.com>
2020-01-15 19:20:51 +0300

    error

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -55,7 +55,7 @@ char				*ft_strnstr(const char *haystack, const char *needle,
 					size_t len);
 int					ft_strcmp(const char *s1, const char *s2);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
-int					ft_atoi(const char *str);
+int					ft_atoi(char *str);
 int					ft_isalpha(int c);
 int					ft_isdigit(int c);
 int					ft_isalnum(int c);
@@ -99,6 +99,6 @@ void				ft_swap(int *a, int *b);
 size_t				ft_slen(const char *s1, const char *s2);
 int					ft_word_count(char const *s, char c);
 int					ft_printf(const char *format, ...);
-void				ft_error1(void);
+void				error(char *str);
 
 #endif
